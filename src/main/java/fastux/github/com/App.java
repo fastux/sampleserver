2b3d2b5e21c71b679c68fa2540170793c7499cdc
fastux <62556252+fastux@users.noreply.github.com>
2020-03-23 23:46:27 +0900

    first version

---
+++ src/main/java/fastux/github/com/App.java
@@ -0,0 +1,13 @@
+package fastux.github.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
